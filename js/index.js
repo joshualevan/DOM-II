a862b1aae8690c3ce0297ca5451219064cb2e4ae
Josh Levan <joshlevan@Joshs-iMac-2.local>
2021-04-06 15:42:59 -0400

    mouseover and keydown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+//Elements
+const links = Array.from(document.links);
+const bus = document.querySelector(".intro img");
+
+//mouse over
+const changeColor = e => e.target.style.color = "red";
+const colorBack = e => e.target.style.color = "black";
+
+links.forEach(link => link.addEventListener('mouseover', changeColor))
+links.forEach(link => link.addEventListener('mouseleave', colorBack))
+
+//keydown
+const enterKey = e => {
+    if(e.key === "Enter"){
+        bus.style.border = "3px solid red";
+    } 
+}
+
+document.addEventListener('keydown', enterKey);
+
+//load
+
+//focus
+
+//resize
+
+//scroll
+
+//select
+
+//dblclick
+
+// drag/ drop
